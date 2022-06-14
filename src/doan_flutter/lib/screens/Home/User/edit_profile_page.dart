b6ac7750914108d5ffb19713e044234ab00e5f69
84348 <thanhlevt7@gmail.com>
2022-06-14 16:42:14 +0700

    load thong tin nguoi dung va chuc nang thay doi thong tin nguoi dung

--- src/doan_flutter/lib/screens/Home/User/edit_profile_page.dart
+++ src/doan_flutter/lib/screens/Home/User/edit_profile_page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:doan_flutter/constant.dart';
+import 'package:doan_flutter/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class EditProfilePage extends StatefulWidget {
@@ -9,14 +12,37 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+  final data = FirebaseFirestore.instance;
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
+
+  updateInformation() async {
+    await data
+        .collection("users")
+        .doc(user!.uid)
+        .update({'fullname': name.text});
+  }
+
   final name = TextEditingController();
-  final gmail = TextEditingController();
+  final email = TextEditingController();
   final phone = TextEditingController();
   final address = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    name.text = "Thành Lễ";
-    gmail.text = "Thanhlevt7@gmail.com";
+    name.text = "${loggedInUser.fullname}";
+    email.text = "${loggedInUser.email}";
     phone.text = "0348340873";
     address.text = "Bà rịa vũng tàu";
     return Scaffold(
@@ -52,22 +78,28 @@ class _EditProfilePageState extends State<EditProfilePage> {
                 )
               ],
             ),
-            const Text("Thành Lễ", style: TextStyle(fontSize: 25)),
+            Text("${loggedInUser.fullname}",
+                style: const TextStyle(fontSize: 25)),
             buildTextField(title: "Tên hiển thị", controller: name, a: false),
-            buildTextField(title: "Gmail", controller: gmail, a: true),
+            buildTextField(title: "Email", controller: email, a: true),
             buildTextField(title: "Số điện thoại", controller: phone, a: false),
             buildTextField(title: "Địa chỉ", controller: address, a: false),
             const SizedBox(
               height: 20.0,
             ),
-            Container(
-              height: 50,
-              width: 250,
-              color: Colors.blue,
-              child: const Center(
-                child: Text(
-                  "Lưu thông tin",
-                  style: TextStyle(fontSize: 25, color: Colors.black),
+            InkWell(
+              onTap: () {
+                updateInformation();
+              },
+              child: Container(
+                height: 50,
+                width: 250,
+                color: Colors.blue,
+                child: const Center(
+                  child: Text(
+                    "Lưu thông tin",
+                    style: TextStyle(fontSize: 25, color: Colors.black),
+                  ),
                 ),
               ),
             ),
