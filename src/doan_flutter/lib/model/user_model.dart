9036a7d7af80c3862d694b9a263dacb3fcedab20
84348 <thanhlevt7@gmail.com>
2022-06-14 17:25:05 +0700

    them thuoc tinh cho model user

--- src/doan_flutter/lib/model/user_model.dart
+++ src/doan_flutter/lib/model/user_model.dart
@@ -3,8 +3,16 @@ class UserModel {
   String? email;
   String? password;
   String? fullname;
+  String? phone;
+  String? address;
 
-  UserModel({this.uid, this.email, this.password, this.fullname});
+  UserModel(
+      {this.uid,
+      this.email,
+      this.password,
+      this.fullname,
+      this.phone,
+      this.address});
 
   factory UserModel.fromMap(map) {
     return UserModel(
@@ -12,6 +20,8 @@ class UserModel {
       email: map['email'],
       password: map['password'],
       fullname: map['fullname'],
+      phone: map['phone'],
+      address: map['address'],
     );
   }
   Map<String, dynamic> toMap() {
@@ -20,6 +30,8 @@ class UserModel {
       'email': email,
       'password': password,
       'fullname': fullname,
-    };  
+      'phone':phone,
+      'address':address,
+    };
   }
 }
