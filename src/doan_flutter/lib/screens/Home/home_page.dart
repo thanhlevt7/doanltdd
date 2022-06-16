9c50f14e7da1b8184b70ae5380c7a064b588550e
84348 <thanhlevt7@gmail.com>
2022-06-16 23:53:29 +0700

    giao dien trang chu

--- src/doan_flutter/lib/screens/Home/home_page.dart
+++ src/doan_flutter/lib/screens/Home/home_page.dart
@@ -22,7 +22,6 @@ class _HomePageState extends State<HomePage> {
               width: 50.0,
               height: 50.0,
             ),
-            
             const Text(
               "Trường Cao đẳng Kỹ thuật CAO THẮNG",
               style: TextStyle(color: Colors.black, fontSize: 16),
@@ -30,16 +29,62 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          listTileCategory("TRANG CHỦ"),
-       
-       
-        ],
-      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          color: const Color(0xFF1FB841),
+          height: 50,
+          width: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: InkWell(
+                  onTap: () => Navigator.pushNamed(context, "/CategoryPage"),
+                  child: Image.asset(
+                    "assets/images/icons/danhmuc.png",
+                    height: 50,
+                  ),
+                ),
+              ),
+              const Text(
+                "TRANG CHỦ",
+                style: TextStyle(fontSize: 25),
+              ),
+              InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/Search");
+                  },
+                  child: const Icon(Icons.search)),
+            ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            "TIN MỚI NHẤT",
+            style: TextStyle(fontSize: 18, color: Colors.red),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: text(
+              "Lịch công tác tuần thứ 40 (Từ ngày 30/5/2022 đến ngày 05/6/2022)\nCuộc thi Solar Boat Challenge 2022\nLịch công tác tuần thứ 39 (Từ ngày 23/5/2022 đến ngày 29/5/2022)\nLịch công tác tuần thứ 38 (Từ ngày 16/5/2022 đến ngày 22/5/2022)\nLịch công tác tuần thứ 40 (Từ ngày 09/5/2022 đến ngày 15/5/2022)"),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            "NGÀNH NGHỀ ĐÀO TẠO",
+            style: TextStyle(fontSize: 18, color: Colors.red),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: text(
+              "Công Nghệ Thông Tin\nKỹ Thuật Cơ Khí\nKỹ Thuật Ô Tô\nKỹ Thuật Cơ Điện tử\nQuản Trị Mạng Máy Tính"),
+        )
+      ]),
     );
   }
-
-
-  
 }
+
